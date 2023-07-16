4807baa6d07ceeb5d0ceb82991193067b9fba2c3
alvarorichard <alvaro10048@protonmail.com>
2023-07-16 16:12:41 -0300

    finish test

--- src/main.c
+++ src/main.c
@@ -12,3 +12,20 @@ int  main(int argc, char **argv)
 
   
 }
+
+void hs_loop(void){
+  char*line;
+  char**args;
+  char status;
+
+  do
+  {
+    printf(">> ");
+    line = hs_read_line();
+    args = hs_split_line(line);
+    status = hs_execute(args);
+    free(line);
+    free(args);
+  } while (status);
+  
+}
