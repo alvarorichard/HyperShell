79c66a221df429fd04a3c8a822ae5858ab848125
alvarorichard <alvaro10048@protonmail.com>
2023-07-16 17:14:17 -0300

    read line

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <stdio.h>
 
 int  main(int argc, char **argv)
 {
@@ -28,4 +29,46 @@ void hs_loop(void){
     free(args);
   } while (status);
   
+}
+
+#define HS_RL_BUFSIZE 1024
+
+char *hs_read_line(void){
+  int bufsize = HS_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "hs: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+ while(1){
+
+  c = getchar();
+
+  if (c == EOF || c == '\n'){
+    buffer[position] = '\0';
+    return buffer;
+  } else {
+    buffer[position] = c;
+  }
+  position++;
+  
+  
+  
+  if(position >= bufsize){
+    bufsize += HS_RL_BUFSIZE;  
+    buffer = realloc(buffer, bufsize);
+    if (!buffer) {
+      fprintf(stderr, "hs: allocation error\n");
+      exit(EXIT_FAILURE);
+
+      
+          }
+ 
+        }
+
+    }
 }
