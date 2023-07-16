d6d70be95a1ec20db9eaaa31f94044702779e054
alvarorichard <alvaro10048@protonmail.com>
2023-07-16 17:57:26 -0300

    launch hs

--- src/main.c
+++ src/main.c
@@ -110,6 +110,28 @@ char **hs_split_line(char *line){
   
 }
 
+int hs_launch(char **args){
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0){
+    if (execvp(args[0],args) == -1){
+      perror("hs");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0){
+    perror("hs");
+  } else {
+    do
+    {
+      wpid = waitpid(pid,&status,WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    
+  }
+  return 1;
+}
+
 int main(int argc, char **argv)
 {
   hs_loop();
